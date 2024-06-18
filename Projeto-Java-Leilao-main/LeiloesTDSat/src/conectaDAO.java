bb205da67c67c9f49a34a7c8d96987e6fab49ce6
Wanderson Felipe <wander1201@hotmail.com>
2024-06-18 15:31:00 -0300

    ajuste conexão com o bd

--- Projeto-Java-Leilao-main/LeiloesTDSat/src/conectaDAO.java
+++ Projeto-Java-Leilao-main/LeiloesTDSat/src/conectaDAO.java
@@ -4,14 +4,6 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-/**
- *
- * @author Adm
- */
 public class conectaDAO {
 
     public Connection getConnectDB() {
@@ -20,7 +12,7 @@ public class conectaDAO {
             Connection conn = DriverManager.getConnection(
                     "jdbc:mysql://localhost:3306/uc11",
                     "root",
-                    "Wanderfelipe13$"
+                    "Wanderfelipe135$"
             );
             return conn;
         } catch (Exception e) {
